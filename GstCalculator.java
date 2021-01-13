81e748843b59a988d31252380d9676d55fca469f
sayali3099 <77379143+sayali3099@users.noreply.github.com>
2021-01-13 12:45:45 +0530

    Add files via upload

---
+++ GstCalculator.java
@@ -0,0 +1,15 @@
+class GstCalculator
+{
+	public static void main(String[] agrs)
+	{
+		double productprice=118;
+		double gstamount=productprice*0.18;
+		double totalamount=productprice*gstamount;
+		
+		System.out.println("total product price is:"+productprice);
+		System.out.println("total amount of product is:"+gstamount);
+		System.out.println("total amount of product is:"+totalamount);
+		
+		
+	}
+}
---
+++ GstExtract.java
@@ -0,0 +1,12 @@
+class GstExtract
+{
+	public static void main(String[] args)
+	{
+		double totalamount=118;
+		double productprice=(totalamount/100)*118;
+		
+		System.out.println("total amount of product is:"+totalamount);
+		System.out.println("total amount of product by extracting gst amount is:"+productprice);
+		
+	}
+}
